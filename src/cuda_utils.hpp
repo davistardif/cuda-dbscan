8ea0e9e02ea3bfff40f068a389266f270d8dc17e
Davis Tardif <davistardif@gmail.com>
2021-05-20 16:04:29 -0700

    Debug

--- src/cuda_utils.hpp
+++ src/cuda_utils.hpp
@@ -1,6 +1,10 @@
 #pragma once
 
 #include <cuda_runtime.h>
+#include <fstream>
+#include <iostream>
+
+using namespace std;
 
 #define CUDA_CALL(ans) { gpuAssert((ans), __FILE__, __LINE__); }
 inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=true)
@@ -13,7 +17,7 @@ inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=t
 }
 
 inline void CUDA_KERNEL_CHECK() {
-    err = cudaGetLastError();
+    cudaError err = cudaGetLastError();
     if  (cudaSuccess != err){
         cerr << "Error " << cudaGetErrorString(err) << endl;
     } else {
--- src/test_delaunay_gpu.cpp
+++ src/test_delaunay_gpu.cpp
@@ -1,7 +1,7 @@
 #include "point_set.hpp"
 #include "clustering.hpp"
 #include "load_taxi.hpp"
-#include "cuda_utils.h"
+#include "cuda_utils.hpp"
 #include "delaunay_gpu.cuh"
 
 #include <iostream>
@@ -9,7 +9,7 @@
 using namespace std;
 
 int main(void) {
-    PointSet ps = get_n_pickups(1024, nullptr);
+    PointSet pts = get_n_pickups(1024, nullptr);
     BBox bbox = pts.extent();
     float max_x = -1000, max_y = -1000; // definitely less than gps coords
     float *dev_max_x, *dev_max_y;
@@ -17,7 +17,8 @@ int main(void) {
     CUDA_CALL(cudaMalloc((void**)dev_max_y, sizeof(float)));
     CUDA_CALL(cudaMemcpy(dev_max_x, &max_x, sizeof(float), cudaMemcpyHostToDevice));
     CUDA_CALL(cudaMemcpy(dev_max_y, &max_y, sizeof(float), cudaMemcpyHostToDevice));
-    cudaCallMaxXYKernel(1024 / 32, 32, ps.data, ps.size * 2, dev_max_x, dev_max_y);
+    cudaCallMaxXYKernel(1024 / 32, 32, pts.data, pts.size * 2,
+                        dev_max_x, dev_max_y);
     CUDA_KERNEL_CHECK();
     CUDA_CALL(cudaMemcpy(&max_x, dev_max_x, sizeof(float), cudaMemcpyDeviceToHost));
     cout << "CPU Result" << bbox.max_x << bbox.max_y << "\n";
