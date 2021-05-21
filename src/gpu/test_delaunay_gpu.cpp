468a9d341e67372d6828b2005bc548339c22766b
Davis Tardif <davistardif@gmail.com>
2021-05-20 17:20:23 -0700

    debug

--- src/gpu/test_delaunay_gpu.cpp
+++ src/gpu/test_delaunay_gpu.cpp
@@ -13,14 +13,21 @@ int main(void) {
     BBox bbox = pts.extent();
     float max_x = -1000, max_y = -1000; // definitely less than gps coords
     float *dev_max_x, *dev_max_y;
+    float *dev_coords;
     CUDA_CALL(cudaMalloc((void**)&dev_max_x, sizeof(float)));
     CUDA_CALL(cudaMalloc((void**)&dev_max_y, sizeof(float)));
+    CUDA_CALL(cudaMalloc((void**)&dev_coords, pts.size * 2 * sizeof(float)));
     CUDA_CALL(cudaMemcpy(dev_max_x, &max_x, sizeof(float), cudaMemcpyHostToDevice));
     CUDA_CALL(cudaMemcpy(dev_max_y, &max_y, sizeof(float), cudaMemcpyHostToDevice));
-    cudaCallMaxXYKernel(1024 / 32, 32, pts.data, pts.size * 2,
+    CUDA_CALL(cudaMemcpy(dev_coords, pts.data, pts.size * 2 * sizeof(float)));
+    cudaCallMaxXYKernel(1024 / 32, 32, dev_coords, pts.size * 2,
                         dev_max_x, dev_max_y);
     CUDA_KERNEL_CHECK();
     CUDA_CALL(cudaMemcpy(&max_x, dev_max_x, sizeof(float), cudaMemcpyDeviceToHost));
+    CUDA_CALL(cudaMemcpy(&max_y, dev_max_y, sizeof(float), cudaMemcpyDeviceToHost));
+    CUDA_CALL(cudaFree(dev_max_x));
+    CUDA_CALL(cudaFree(dev_max_y));
+    CUDA_CALL(cudaFree(dev_coords));
     cout << "CPU Result" << bbox.max_x << bbox.max_y << "\n";
     cout << "GPU Result" << max_x << max_y << "\n";
     return 0;
