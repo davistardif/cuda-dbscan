3b9ef3473820a24674a7fbe659fc21500dfca556
Davis Tardif <davistardif@gmail.com>
2021-06-03 13:20:32 -0700

    use cpu for marking core points

--- src/gpu/delaunay.cu
+++ src/gpu/delaunay.cu
@@ -15,6 +15,7 @@
 #include <cmath>
 #include <cassert>
 #include <vector>
+#include <unordered_map>
 
 #define SQRT_2 (1.4142135623)
 
@@ -43,63 +44,75 @@ Clustering *delaunay_dbscan(PointSet &pts, float epsilon, unsigned int min_point
     callGridLabelKernel(blocks, threadsPerBlock, dev_pt_ids, dev_grid_labels,
                         dev_coords, bbox.min_x, bbox.min_y, side_len,
                         grid_x_size, pts.size);
-
+    uint *grid_labels = (uint *) malloc(pts.size * sizeof(uint));
+    CUDA_CALL(cudaMemcpy(grid_labels, dev_grid_labels, pts.size * sizeof(uint), cudaMemcpyDeviceToHost));
     // Insert into hash table
-    CUDPPHandle cudpp;
-    CUDPP_CALL(cudppCreate(&cudpp));
-    CUDPPHashTableConfig *hashconf = new CUDPPHashTableConfig;
-    *hashconf = {
-        CUDPP_MULTIVALUE_HASH_TABLE,
-        (uint) pts.size,
-        1.25 // extra memory factor (1.05 to 2.0, trades memory for build speed)
-    };
-    CUDPPHandle grid;
-    CUDPP_CALL(cudppHashTable(cudpp, &grid, hashconf));
-    CUDPP_CALL(cudppHashInsert(grid, dev_grid_labels, dev_pt_ids, pts.size));
-
-    // Mark core points where cell has >= min points
-    unsigned int *d_values;
-    CUDA_CALL(cudaMalloc((void**)&d_values, pts.size * sizeof(unsigned int)));
-    CUDPP_CALL(cudppMultivalueHashGetAllValues(grid, &d_values));
-    unsigned int unique_key_count;
-    CUDPP_CALL(cudppMultivalueHashGetUniqueKeyCount(grid, &unique_key_count));
-    unsigned int *d_index_counts;
-    CUDA_CALL(cudaMalloc((void**)&d_index_counts, unique_key_count * 2 * sizeof(unsigned int)));
-    CUDPP_CALL(cudppMultivalueHashGetIndexCounts(grid, &d_index_counts));
-    
-    bool *d_isCore, *isCore;
-    CUDA_CALL(cudaMalloc((void**)&d_isCore, pts.size * sizeof(bool)));
-    CUDA_CALL(cudaMemset(d_isCore, 0, pts.size * sizeof(bool)));
-    callGridMarkCoreCells(blocks, threadsPerBlock, d_index_counts,
-                          unique_key_count, d_values, d_isCore, min_points);
-    
-    // Find remaining core points
-    isCore = (bool *) malloc(pts.size * sizeof(bool));
-    assert(isCore != nullptr);
-    CUDA_CALL(cudaMemcpy(isCore, d_isCore, pts.size * sizeof(bool), cudaMemcpyDeviceToHost));
-    uint *d_query_keys, *d_results;
-    CUDA_CALL(cudaMalloc((void**)&d_query_keys, 21 * sizeof(uint)));
-    CUDA_CALL(cudaMalloc((void**)&d_results, 21 * sizeof(uint2)));
-    for (int i = 0; i < pts.size; i++) {
-        if (!isCore[i]) {
-            int x = (int) ((pts.get_x(i) - bbox.min_x) / side_len);
-            int y = (int) ((pts.get_y(i) - bbox.min_y) / side_len);
-            vector<int> ncells = neighbor_cell_ids(x, y, grid_x_size, grid_y_size);
-            CUDA_CALL(cudaMemcpy(d_query_keys, (uint *) ncells.data(),
-                                 ncells.size() * sizeof(uint), cudaMemcpyHostToDevice));
-            CUDPP_CALL(cudppHashRetrieve(grid, d_query_keys, d_results, ncells.size()));
-            callGridCheckCore(dev_coords, d_results, ncells.size(), d_values,
-                              d_isCore, min_points, EPS_SQ,
-                              pts.get_x(i), pts.get_y(i), i);
+    unordered_map<uint, vector<uint>> grid;
+    for (uint i = 0; i < pts.size; i++) {
+      uint idx = grid_labels[i];
+      auto it = grid.find(idx);
+        if (it == grid.end()) {
+            grid[idx] = vector<uint>();
+            grid[idx].push_back(i);
+        }
+        else {
+            it->second.push_back(i);
         }
     }
-    CUDA_CALL(cudaMemcpy(isCore, d_isCore, pts.size * sizeof(bool), cudaMemcpyDeviceToHost));
+    
+    // Mark core
+    int num_core = 0, num_core_cells = 0;
+    bool *is_core = (bool*) malloc(pts.size * sizeof(bool));
+    assert(is_core != nullptr);
+    memset(is_core, 0, pts.size * sizeof(bool));
+    unordered_map<uint, uint> core_cell_idx;
+    for (auto it = grid.begin(); it != grid.end(); it++) {
+        if (it->second.size() >= min_points) {
+            // mark all points in the cell as core points
+            core_cell_idx[it->first] = num_core_cells++;
+            for (int i = 0; i < it->second.size(); i++) {
+                is_core[it->second[i]] = true;
+            }
+            num_core += it->second.size();
+            continue;
+        }
+        // otherwise, need to check each point individually
+        uint x = it->first % grid_x_size;
+        uint y = it->first / grid_x_size;
+        bool cell_has_core = false;
+        for (uint i = 0; i < it->second.size(); i++) {
+            // scan over neighboring cells (anything else is too far away to matter)
+            uint pt1 = it->second[i];
+            vector<int> nbrs = neighbor_cell_ids(grid, x, y, grid_x_size, grid_y_size);
+            // automatically include all points in the same grid cell
+            int nbr_count = it->second.size();
+            int j = 0;
+            while (nbr_count < min_points && j < nbrs.size()) {
+                vector<uint> to_check = grid[nbrs[j]];
+                for (uint &pt2 : to_check) {
+                    if (pts.dist_sq(pt1, pt2) <= EPS_SQ)
+                        nbr_count++;
+                }
+                j++;
+            }
+            if (nbr_count >= min_points) {
+                is_core[pt1] = true;
+                num_core++;
+                if (!cell_has_core) {
+                    core_cell_idx[it->first] = num_core_cells++;
+                    cell_has_core = true;
+                }
+            }
+        }
+    }
     // Delaunay triangulation of core points
     Point2HVec core_pts;
-    vector<int> core_idx;
+    vector<uint> core_idx;
+    core_pts.reserve(num_core);
+    core_idx.reserve(num_core);
     Point2 p;
-    for (int i = 0; i < pts.size; i++) {
-        if (isCore[i]) {
+    for (uint i = 0; i < pts.size; i++) {
+        if (is_core[i]) {
             p._p[0] = pts.get_x(i);
             p._p[1] = pts.get_y(i);
             core_pts.push_back(p);
@@ -112,9 +125,9 @@ Clustering *delaunay_dbscan(PointSet &pts, float epsilon, unsigned int min_point
     GDel2DOutput gdelOut;
     gdel.compute(gdelIn, &gdelOut);
               
-    CUDPP_CALL(cudppDestroyHashTable(cudpp, grid));
+    //CUDPP_CALL(cudppDestroyHashTable(cudpp, grid));
     
-    CUDPP_CALL(cudppDestroy(cudpp));
+    //CUDPP_CALL(cudppDestroy(cudpp));
     CUDA_CALL(cudaFree(dev_coords));
     return clusters;
 }
@@ -147,11 +160,12 @@ BBox cuda_extent(PointSet &pts, float *dev_coords,
     return bbox;
 }
 
+/*
 vector<int> neighbor_cell_ids(int x, int y, int grid_x_size, int grid_y_size) {
     /* Returns a vector of cell id's in grid which are epsilon neighbors
        of (x,y) and not out of bounds.
        includes (x,y) itself
-    */
+    * /
     int cell_r_c[] = {
         (y-2), x - 1,
         (y-2), x,
@@ -186,6 +200,51 @@ vector<int> neighbor_cell_ids(int x, int y, int grid_x_size, int grid_y_size) {
         }
         int id = r * grid_x_size + c;
         valid_cells.push_back(id);
+    }
+    return valid_cells;
+}
+*/
+vector<int> neighbor_cell_ids(unordered_map<uint, vector<uint>> &grid,
+                              int x, int y, int grid_x_size, int grid_y_size) {
+    /* Returns a vector of cell id's in grid which are epsilon neighbors
+       of (x,y) and are non-empty
+       Note: does not include (x,y) itself
+    */
+    int cell_r_c[] = {
+        (y-2), x - 1,
+        (y-2), x,
+        (y-2), x + 1,
+        (y-1), x - 2,
+        (y-1), x - 1,
+        (y-1), x,
+        (y-1), x + 1,
+        (y-1), x + 2,
+        y, x - 2,
+        y, x - 1,
+        y, x + 1,
+        y, x + 2,
+        (y+2), x - 1,
+        (y+2), x,
+        (y+2), x + 1,
+        (y+1), x - 2,
+        (y+1), x - 1,
+        (y+1), x,
+        (y+1), x + 1,
+        (y+1), x + 2
+    };
+    vector<int> valid_cells;
+    for (int i = 0; i < 20; i++) { // 20 neighbor cell possibilities
+        int r = cell_r_c[i*2];
+        int c = cell_r_c[i*2 + 1];
+        if (c < 0 || r < 0 || c >= grid_x_size || r >= grid_y_size) {
+            // out of bounds
+            continue;
+        }
+        int id = r * grid_x_size + c;
+        if (grid.count(id) > 0) {
+            // only return nonempty cells
+            valid_cells.push_back(id);
+        }
     }
     return valid_cells;
 }
--- src/gpu/delaunay.cuh
+++ src/gpu/delaunay.cuh
@@ -2,6 +2,9 @@
 #include "point_set.hpp"
 
 #include <vector>
+#include <unordered_map>
+
+using namespace std;
 
 Clustering *delaunay_dbscan(PointSet &pts, float epsilon, unsigned int min_points);
 
@@ -9,4 +12,6 @@ BBox cuda_extent(PointSet &pts, float *dev_coords,
                  const unsigned int blocks,
                  const unsigned int threadsPerBlock);
 
-std::vector<int> neighbor_cell_ids(int x, int y, int grid_x_size, int grid_y_size);
+//std::vector<int> neighbor_cell_ids(int x, int y, int grid_x_size, int grid_y_size);
+vector<int> neighbor_cell_ids(unordered_map<uint, vector<uint>> &grid,
+                              int x, int y, int grid_x_size, int grid_y_size);
