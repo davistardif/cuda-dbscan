acdb97afa7eaff0f4e58ad00a73904878160b8ec
Davis Tardif <davistardif@gmail.com>
2021-06-03 15:46:29 -0700

    begin cmdline argparsing

--- src/gpu/main.cpp
+++ src/gpu/main.cpp
@@ -3,9 +3,11 @@
 #include "load_taxi.hpp"
 #include "cuda_utils.hpp"
 #include "delaunay.cuh"
+#include "cmdline_parse.hpp"
 
-int main(void) {
+int main(int argc, char **argv) {
     PointSet pts = get_n_pickups(1024, nullptr);
-    delaunay_dbscan(pts, 0.1, 100);
+    Clustering c = delaunay_dbscan(pts, 0.1, 100);
+    c.print();
     return 0;
 }
